77523bedbdb22224a3d278eac02ddf485413a11e
Ionel Fona <ionel@provoker.io>
2024-03-07 16:40:25 +0200

    Tweak the start view to have an action <a> instead of an <a> within a <button>.

--- resources/views/personality-test/start.blade.php
+++ resources/views/personality-test/start.blade.php
@@ -3,28 +3,28 @@
 @section('title', 'Personality Test - Start')
 
 @section('content')
-    <div class="max-w-xs mt-8">
-        <span class="font-bold">Hello, {{ $student->first_name . ' ' . $student->last_name }}</span>
+    <div class="max-w-xs">
+        <div class="mt-8">
+            <span class="font-bold">Hello, {{ $student->first_name . ' ' . $student->last_name }}</span>
 
-        <p class="mt-2">
-            This test will take approximately 40-45 minutes. Upon completion, you will receive a detailed report, a RIASEC score and a list of potentially interesting careers that might match your personality profile.
-        </p>
-    </div>
+            <p class="mt-2">
+                This test will take approximately 40-45 minutes. Upon completion, you will receive a detailed report, a RIASEC score and a list of potentially interesting careers that might match your personality profile.
+            </p>
+        </div>
 
-    <div class="max-w-xs mt-8">
-        <span class="font-bold">How to complete the test?</span>
+        <div class="mt-8">
+            <span class="font-bold">How to complete the test?</span>
 
-        <p class="mt-2">
-            Mark each statement on a scale from “Disagree strongly” to “Agree strongly”.
-        </p>
-    </div>
+            <p class="mt-2">
+                Mark each statement on a scale from “Disagree strongly” to “Agree strongly”.
+            </p>
+        </div>
 
-    <div class="max-w-xs mt-8 flex items-center justify-center">
-        <button class="bg-primary uppercase px-12 py-4 rounded-full font-bold">
-            <a href="{{ route('personality-test.index', ['studentUid' => $student->uid, 'showQuestions' => true]) }}"
-               class="text-gray-primary">
+        <div class="mt-8 flex items-center justify-center">
+            <a class="bg-primary uppercase px-12 py-4 rounded-full font-bold text-gray-primary"
+               href="{{ route('personality-test.index', ['studentUid' => $student->uid, 'showQuestions' => true]) }}">
                 START TEST
             </a>
-        </button>
+        </div>
     </div>
 @endsection
