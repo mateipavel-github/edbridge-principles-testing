a591d7e93d7faaf3fbaab5fd152f7bb363ae8da8
Matei Pavel <matei+github@konk-media.com>
2025-02-05 21:59:38 +0200

    add getJson function and api route to load the json in the prompt editor

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/save-career-report-template', [CareerReportController::class, 'storeJson']);
+Route::get('/get-career-report-template/{name}', [CareerReportController::class, 'getJson']);
 Route::get('/career-report/{accountId}/{careerTitle}', [CareerReportController::class, 'generateCareerReport']);
