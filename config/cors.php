b23ef022e3317193c022ee3c64ea985b7a8bea0e
Matei Pavel <matei+github@konk-media.com>
2025-02-12 18:26:59 +0200

    added CORS policy to work from localhost

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:8000', 'http://localhost'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
